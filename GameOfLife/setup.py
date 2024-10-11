113713c28f2399ef040d44436bcd2c41af51af3e
K-a-r-e-e-m <Kareem.H1715@gmail.com>
2024-10-12 01:15:04 +0300

    fix: make setup can run on posix or nt operating systems

--- GameOfLife/setup.py
+++ GameOfLife/setup.py
@@ -1,4 +1,8 @@
+import os
 from cx_Freeze import setup, Executable
+
+# Determine the operating system (posix) or (nt)
+is_windows = os.name == 'nt' # Check if it windows assign true else false
 
 # Define the build options, this will include any necessary packages that may not be detected automatically.
 build_exe_options = {
@@ -11,11 +15,14 @@ build_exe_options = {
     ]
 }
 
+# Set target name based on the operating system
+target_name = "TheGameOfLife.exe" if is_windows else "TheGameOfLife"
+
 # Define the setup
 setup(
     name="The Game of Life",
     version="1.0",
     description="Game of Life Application",
     options={"build_exe": build_exe_options},
-    executables=[Executable("app.py", target_name="TheGameOfLife")],  # Entry point script
+    executables=[Executable("app.py", target_name=target_name)],
 )
